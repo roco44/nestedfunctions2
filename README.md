12ac1b8f7a4eaf2d0db8bf9525775d0878abdc9f
roco44 <rgcinter@gmail.com>
2023-05-27 18:07:12 +0100

    Add exercises from TutorialsPoint, ok after I debugged it! Also TutorialsPoint and tektutorialshub

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # nestedfunctions2
 further exercises in jscript nested functions
 "Starting with Educba triangleH but not working yet"
+"Add exercises from TutorialsPoint, ok after I debugged it! Also TutorialsPoint and tektutorialshub"
--- script.js
+++ script.js
@@ -1,26 +1,123 @@
 
 //From Educba
-//Outer function() with single inner function()
-function triangleHypotenuse(base, height)
-{
-    function square (side, side)
-    {
-    return side * side;
-    }
+// Have given up on Educba exercises due to document.write commands not working!
+//Back to Java*Script Info
+// function sayHiBye(firstName, lastName) {
 
-    return Math.sqrt(square(base) + square(height));
-}
-    //document.write("The hypotenuse of this triangle is: " + triangleHypotenuse(3,4));
-    //alert(triangleHypotenuse(3,4));
-    console.log(triangleHypotenuse(3,4))
+//     // helper nested function to use below
+//     function getFullName() {
+//       return firstName + " " + lastName;
+//     }
+  
+//     alert( "Hello, " + getFullName() );
+//     alert( "Bye, " + getFullName() );
+  
+//   }
 
+//     sayHiBye("Finn", "Jackson")
+//     sayHiBye("Freya", "Jackson")
+//     sayHiBye("Shona", "Jackson-Russell")
+//     sayHiBye("Ben", "Jackson")
+//----------------------------------------
 
-// function triangleHypotenuse(base,height)
+// function makeCounter() {
+//     let count = 0;
+  
+//     return function() {
+//       return count++;
+//     };
+//   }
+  
+//   let counter = makeCounter();
+  
+//   alert( counter() ); // 0
+//   alert( counter() ); // 1
+//   alert( counter() ); // 2
+  //--------------------------
+
+//TutorialsPoint
+// function add_two(a,b) {
+//     let result =a+b;
+    
+//     //function add_three(c)   PS I debugged this !
+//     {
+//        function add_three(c) {
+//           result = result+c;
+//        }
+//        add_three(2); 
+//        return result;
+//     }
+//  }
+//  let answer = add_two(22,6);
+//  document.getElementById("1demo").innerHTML = answer;
+//------------------------------------------------------
+
+//tektutorialshub
+//version without nested function
+// function addnum(a,b)
 // {
-// function square(side){
-// return side*side;
+//     return (a + b);
 // }
-// return Math.sqrt(square(base)+square(height));
+//     console.log("this result addnum is: " + addnum(1,2))
+// //---------------------------
+// //version with nested function
+// function addNum(a,b)
+// {
+//   //nested function
+//   function logToConsole(message)
+//   {
+//     console.log(message);
+//   }
+//   let result=a+b;
+//   //invoking the nested function
+//   logToConsole("and this result addNum is: "+result)
 // }
-// document.write("Hypotenuse of triangle is :"+triangleHypotenuse(3,4));
+//   addNum(1,2)
+
+//--------------------------------------
+
+//with lexical scope
+// function addNum(a,b)
+// {
+
+//   function logToConsole()
+//   {
+//     console.log(`result of %d + %d is %d`,a ,b,result);
+//   }
+
+//   let result=a+b;
+//   logToConsole()
+// }
+
+// addNum(11 ,21)
+//-------------------------------------------------------
+
+// function makeCounter() {
+//     let count = 0;
+
+//     increment = function () {
+//         return ++count;
+//     };
+
+//     return increment;
+// }
+
+// //storing the inner or nested function
+// counter = makeCounter()
+
+
+// //Invoking the nested function. This will increment the count property by 1
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+//----------------------
+
+
+  
+
+
+
+
+
 
